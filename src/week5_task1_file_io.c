fdab7da878c6b96d1832249534877e2bb954cbd8
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-10-21 22:01:22 +0300

    finished 5_1

--- src/week5_task1_file_io.c
+++ src/week5_task1_file_io.c
@@ -12,12 +12,30 @@ int main(void) {
     char line[256];
 
     // TODO: 1. Open file for writing (mode = "w")
+    fp = fopen(filename, "w");
     // TODO: 2. Check if file opened successfully
+    if (fp == NULL){
+        fprintf(stderr, "Error opening file");
+        return 1;
+    }
     // TODO: 3. Write 2–3 lines of text to the file using fprintf()
+    char str[50];
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Enter line %d", i);
+        scanf("%s", str);
+        printf("\n");
+        scanf("%[^\n]%*c", str);
+        fprintf(fp,"%d.%s\n", i, str);
+    }
+    
     // TODO: 4. Close the file
+    flocse(fp);
 
     // TODO: 5. Open file again for reading (mode = "r")
+    fp = fopen(filename, "r");
     // TODO: 6. Use fgets() in a loop to read and print each line to the console
+    
     // TODO: 7. Close the file
 
     // BONUS: ask user for filename instead of using default "data.txt"
